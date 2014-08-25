f2b72fa62fc3a336e34859f56bbd5308a9beb84d
Erin Hinkle <erinhinx5@gmail.com>
2014-08-25 13:28:30 -0400

    mouseleave

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$(".ryu").mouseenter(function() {
-		alert("mouse entered .ryu div");
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
 	})
 	.mouseleave(function() {
-		alert("mouse left");
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
 	});
 })
 
